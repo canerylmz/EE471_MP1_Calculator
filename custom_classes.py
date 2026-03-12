afddd60e7699e28b67fbc7c19520f2f14b29d6bf
Caner Yilmaz <caneryilmaz57@gmail.com>
2026-03-12 17:06:48 +0300

    multiply method added

--- custom_classes.py
+++ custom_classes.py
@@ -1,3 +1,8 @@
 class Calculator:
     def __init__(self):
         self._current_val = 0
+    
+    def multiply(self,x,y):
+        product = x * y
+        self._current_val = product
+        return product
